4b33d0195477e391f8a206dd020801147c6433bf
lakhankumar <lakhankumar997@gmail.com>
2021-09-24 18:43:25 +0530

    Returns total fare, total no.of rides and average fare

--- test/java/CabInvoiceGeneratorTest.java
+++ test/java/CabInvoiceGeneratorTest.java
@@ -1,27 +1,33 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception{
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double result = cabInvoiceGenerator.calculateFare(15, 30);
         Assert.assertEquals(180, result,0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double result = cabInvoiceGenerator.calculateFare(0.1, 1);
         Assert.assertEquals(5, result,0);
     }
 
     @Test
-    public void givenMultipleDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    public void givenMultipleDistanceAndTime_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(2.0,5),
-                new Ride(0.1,1)};
-        double result = cabInvoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30,result,0);
+                        new Ride(0.1,1)};
+        InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary,invoiceSummary);
     }
 }
